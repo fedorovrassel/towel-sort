0ee87f777753900357ace25bfe4ce094cd91979f
Your Name <you@example.com>
2022-08-20 19:11:55 +0400

    feat: latest stable version of index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let newMatrix = [];
+  if (matrix.length == 0 || matrix == []) {return([])} else {
+  for (let i = 0; i < matrix.length; i++) {
+    if (i%2!==0) {newMatrix[i]=matrix[i].reverse();}
+    else {newMatrix[i]=matrix[i];}
+  }
+  let array = newMatrix.map(arr => arr.join(',')).join(',').split(',');
+  let res = array.map(arr=>Number(arr));
+  return(res);
+  }
 }
